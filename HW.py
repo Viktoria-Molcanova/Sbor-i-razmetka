3f982159f1261509c8510321f5661491eb0b54d4
Viktoria-Molcanova <119111084+Viktoria-Molcanova@users.noreply.github.com>
2024-09-02 21:47:40 +0700

    Update HW.py

--- HW.py
+++ HW.py
@@ -36,6 +36,7 @@ params = {
 }
 
 headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36"
     "Accept": "application/json",
     "Authorization": os.getenv("Authorization")
 }
